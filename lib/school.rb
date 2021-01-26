50b864fdb8e37048dae25e400ccff97fcaacc715
Hatice <haticekaraty@gmail.com>
2021-01-25 20:09:17 -0500

    commit tree 860728c9b263961239d268056c8136aa7fbc7282

--- lib/school.rb
+++ lib/school.rb
@@ -7,33 +7,33 @@ class School
         @name = name
         @roster = {}
     end  
-    
+   
     def add_student(student_name, grade)
-        @roster[grade] = []  # first, key_value pair. At key_grade set an empty array to store students names
-        @roster.each do |grade_key,value_name|  # loop through each key value pair on the roster hash
-            if grade  == grade_key              # if grade passes as argument is already exist in the hash, push student_name to the array 
-                @roster[grade] << student_name
-            else                                # if no key existed at the grade level and then create a new key at that grade.
-                @roster[grade] = []
-                @roster[grade] << student_name 
+        # Ruby doesn't let you
+        #add new keys to the hash when iterating
+        #As a solution easily duplicate hash ---> clone
+       #@roster.clone.each do |grade_key, value_name|
+            if @roster[grade]
+                @roster[grade]<<student_name
+            else
+                @roster[grade]=[student_name]
             end
-        end
+        #alternate solution to add_student method
+        #@roster[grade] ||= []
+       #@roster[grade] << student_name
+        #end
+       @roster
     end
 
     def grade(student_grade)
         @roster[student_grade]
     end
 
-   
-end
-=begin
-def grade(student_grade)
-        @roster[student_grade]
-    end
-
     def sort
-        sorted_array = []
-        @roster.each do |grade,student_name|
-        value.sort
-
-=end
+        new_roster = {}
+        @roster.sort.to_h.each do |grade_key, name_value|
+           new_roster[grade_key] = name_value.sort
+        end
+        new_roster
+    end
+end
